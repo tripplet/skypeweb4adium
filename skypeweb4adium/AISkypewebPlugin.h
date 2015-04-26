c3e7b2f59e0d5e65bb1fabc87831e4d0fd4bacf6
Tobias Tangemann <tobias@tangemann.org>
2015-04-26 21:26:15 +0200

    Basic structure

---
+++ skypeweb4adium/AISkypewebPlugin.h
@@ -0,0 +1,17 @@
+//
+//  AISkypewebPlugin.h
+//  skypeweb4adium
+//
+//  Created by Tobias Tangemann on 26.04.15.
+//  Copyright (c) 2015 Tobias Tangemann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <Adium/AIPlugin.h>
+#import <AdiumLibpurple/AILibpurplePlugin.h>
+
+@interface AISkypewebPlugin : AIPlugin <AILibpurplePlugin> {
+}
+
+@end
---
+++ skypeweb4adium/AISkypewebPlugin.m
@@ -0,0 +1,44 @@
+//
+//  AISkypeWebPlugin.m
+//  skypeweb4adium
+//
+//  Created by Tobias Tangemann on 26.04.15.
+//  Copyright (c) 2015 Tobias Tangemann. All rights reserved.
+//
+
+#import "AISkypewebPlugin.h"
+
+@implementation AISkypewebPlugin
+
+- (void)installPlugin
+{
+}
+
+- (void)uninstallPlugin
+{
+}
+
+- (void)installLibpurplePlugin
+{
+}
+
+- (void)loadLibpurplePlugin
+{
+}
+
+- (NSString *)pluginAuthor
+{
+  return @"Tobias Tangemann";
+}
+
+-(NSString *)pluginVersion
+{
+  return @"1.0";
+}
+
+-(NSString *)pluginDescription
+{
+  return @"SkypeWeb";
+}
+
+@end
