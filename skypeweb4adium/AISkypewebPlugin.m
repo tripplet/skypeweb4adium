4cc54ae9f3b06654ed6a2c7908b1acf552082547
Tobias Tangemann <tobiastangemann@gmail.com>
2016-01-29 10:50:02 +0100

    Small code optimization

--- skypeweb4adium/AISkypewebPlugin.m
+++ skypeweb4adium/AISkypewebPlugin.m
@@ -22,32 +22,17 @@ extern void purple_init_skypeweb_plugin();
     [PurpleSkypeWebService registerService];
 }
 
-- (void)uninstallPlugin
-{
-}
-
-- (void)installLibpurplePlugin
-{
-}
-
 - (void)loadLibpurplePlugin
 {
     AILog(@"Loading skypeweb4adium plugin %s (skypeweb4pidgin: "SKYPEWEB_PLUGIN_VERSION")",[[self pluginVersion] UTF8String]);
 }
 
-- (NSString *)pluginAuthor
-{
-  return @"Tobias Tangemann";
-}
+- (NSString *)pluginAuthor      { return @"Tobias Tangemann"; }
+- (NSString *)pluginDescription { return @"SkypeWeb"; }
+- (NSString *)pluginURL         { return @"https://github.com/tripplet/skypeweb4adium#readme"; }
+- (NSString *)pluginVersion     { return [[[NSBundle bundleForClass:[self class]] infoDictionary] objectForKey:@"CFBundleVersion"]; }
 
--(NSString *)pluginVersion
-{
-  return @"1.1";
-}
-
--(NSString *)pluginDescription
-{
-  return @"SkypeWeb";
-}
+- (void)uninstallPlugin {}
+- (void)installLibpurplePlugin {}
 
 @end
